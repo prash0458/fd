258c941d9094bb29a7bbba05ad9f0b713575d062
prash0458 <prashanth0458@gmail.com>
2021-10-29 22:57:58 -0400

    format on save

--- ClientApp/src/app/counter/counter.component.ts
+++ ClientApp/src/app/counter/counter.component.ts
@@ -7,20 +7,32 @@ import { increment,decrement,reset } from './counter.actions';
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+export class CounterComponent
+{
   count$: any;
-        
-  constructor(private store: Store<{count:number}>){
+             
+  constructor(private store: Store<{ count: number }>)
+  {
     this.count$ = store.select('count');
   }
   public currentCount = 0;
 
-increment(){ this.store.dispatch(increment());}
-decrement(){ this.store.dispatch(decrement());}
-reset(){this.store.dispatch(reset());}
+  increment()
+  {
+    this.store.dispatch(increment());
+  }
+  decrement()
+  {
+    this.store.dispatch(decrement());
+  }
+  
+  reset()
+  {
+    this.store.dispatch(reset());
+  }
  
-
-  public incrementCounter() {
+  public incrementCounter()
+  {
     this.currentCount++;
   }
 }
