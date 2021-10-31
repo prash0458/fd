0f472a205258c2a2aa30456a25dce4bcd8992fbd
prash0458 <prashanth0458@gmail.com>
2021-10-31 19:54:19 -0400

    added routing

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { BookComponent } from "./book/book.component";
+import { CategoryComponent } from "./category/category.component";
+import { CounterComponent } from "./counter/counter.component";
+import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 import { HomeComponent } from "./home/home.component";
 import { LetterModule } from "./letter/letter.module";
 
@@ -10,6 +14,11 @@ const routes: Routes = [
     loadChildren: () =>
       import("./letter/letter.module").then((m) => m.LetterModule),
   },
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "counter", component: CounterComponent },
+  { path: "fetch-data", component: FetchDataComponent },
+  { path: "category", component: CategoryComponent },
+  { path: "book", component: BookComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/src/app/letter/form/form-one/form-one.component.ts
+++ ClientApp/src/app/letter/form/form-one/form-one.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-form-one',
-  templateUrl: './form-one.component.html',
-  styleUrls: ['./form-one.component.css']
+  selector: "app-form-one",
+  templateUrl: "./form-one.component.html",
+  styleUrls: ["./form-one.component.css"],
 })
 export class FormOneComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
